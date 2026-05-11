2724e2cc2f989e55228f928c8f37de12a9ce86f1
emmachen2100 <emmachen2100@gmail.com>
2026-05-10 21:08:34 -0400

    Set Roblox board image asset

--- roblox/src/shared/AssetConfig.lua
+++ roblox/src/shared/AssetConfig.lua
@@ -2,6 +2,6 @@ local AssetConfig = {}
 
 -- Upload a PNG export of the web board to Roblox, then paste the image id here.
 -- Example: "rbxassetid://1234567890"
-AssetConfig.BoardImage = ""
+AssetConfig.BoardImage = "rbxassetid://115908955193622"
 
 return AssetConfig
